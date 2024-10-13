b09977ed68353d4610cd455ed6b0a04d4b81fe68
JustDooooIt <844730658@qq.com>
2024-10-14 00:30:20 +0800

    update

--- include/support/instance_info.hpp
+++ include/support/instance_info.hpp
@@ -10,8 +10,13 @@ GDExtensionBool get_func(GDExtensionScriptInstanceDataPtr p_instance, GDExtensio
 // void free_property_list_func(GDExtensionScriptInstanceDataPtr p_instance, const GDExtensionPropertyInfo *p_list, uint32_t p_count);
 // GDExtensionBool property_can_revert_func(GDExtensionScriptInstanceDataPtr p_instance, GDExtensionConstStringNamePtr p_name);
 // GDExtensionBool property_get_revert_func(GDExtensionScriptInstanceDataPtr p_instance, GDExtensionConstStringNamePtr p_name, GDExtensionVariantPtr r_ret);
+GDExtensionBool has_method_func(GDExtensionScriptInstanceDataPtr p_instance, GDExtensionConstStringNamePtr p_name);
+GDExtensionInt get_method_argument_count_func(GDExtensionScriptInstanceDataPtr p_instance, GDExtensionConstStringNamePtr p_name, GDExtensionBool *r_is_valid);
 void call_func(GDExtensionScriptInstanceDataPtr p_self, GDExtensionConstStringNamePtr p_method, const GDExtensionConstVariantPtr *p_args, GDExtensionInt p_argument_count, GDExtensionVariantPtr r_return, GDExtensionCallError *r_error);
 void notification_func(GDExtensionScriptInstanceDataPtr p_instance, int32_t p_what, GDExtensionBool p_reversed);
+void to_string_func(GDExtensionScriptInstanceDataPtr p_instance, GDExtensionBool *r_is_valid, GDExtensionStringPtr r_out);
+void refcount_incremented_func(GDExtensionScriptInstanceDataPtr p_instance);
+GDExtensionBool refcount_decremented_func(GDExtensionScriptInstanceDataPtr p_instance);
 GDExtensionObjectPtr get_owner_func(GDExtensionScriptInstanceDataPtr p_instance);
 GDExtensionObjectPtr get_script_func(GDExtensionScriptInstanceDataPtr p_instance);
 GDExtensionScriptLanguagePtr get_language_func(GDExtensionScriptInstanceDataPtr p_instance);
@@ -31,13 +36,13 @@ static const GDExtensionScriptInstanceInfo3 InstanceInfo{
 	nullptr,
 	nullptr,
 	nullptr,
-	nullptr,
-	nullptr,
+	has_method_func,
+	get_method_argument_count_func,
 	call_func,
 	notification_func,
-	nullptr,
-	nullptr,
-	nullptr,
+	to_string_func,
+	refcount_incremented_func,
+	refcount_decremented_func,
 	get_script_func,
 	nullptr,
 	nullptr,
