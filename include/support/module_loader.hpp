fedc9580dab33268124a1390f130072b9208052a
moluopro <moluopro@qq.com>
2024-11-06 21:11:40 +0800

    cache rm

--- include/support/module_loader.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef __MODULE_LOADER_H__
-#define __MODULE_LOADER_H__
-
-typedef struct JSModuleDef JSModuleDef;
-typedef struct JSContext JSContext;
-
-JSModuleDef *module_loader(JSContext *ctx, const char *module_name, void *opaque);
-#endif // __MODULE_LOADER_H__
--- test/dict_test/dict_test.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef __DICT_TEST_H__
-#define __DICT_TEST_H__
-
-void test_dict();
-
-#endif // __DICT_TEST_H__
