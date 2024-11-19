2c3c086ad4b74c93e503906ce6343eb29f84c4fa
moluopro <moluopro@qq.com>
2024-11-19 21:59:47 +0800

    update compile.ps1

--- compile.ps1
+++ compile.ps1
@@ -8,4 +8,43 @@ if ($args.Count -eq 0 -or $args[0] -match "none") {
     git clone https://github.com/godotengine/godot-cpp -b $args[0]
 }
 
+$pip = if ($IsLinux) { "pip3" } else { "pip" }
+$python = if ($IsLinux) { "python3" } else { "python" }
+
+function check_python {
+
+    if (-not (Get-Command $python -ErrorAction SilentlyContinue)) {
+        echo "Python is not installed."
+        exit 1
+    }
+
+    if (-not (Get-Command $pip -ErrorAction SilentlyContinue)) {
+        echo "Pip is not installed."
+        exit 1
+    }
+
+    try {
+        & $pip show jinja2 > $null
+        if ($LASTEXITCODE -ne 0) {
+            echo "Please install python3's jinja2 package . "
+            # & $pip install jinja2
+            # sudo apt install python3-jinja2
+        }
+    }
+    catch {
+        echo "Failed to install jinja2."
+        exit 1
+    }
+
+}
+
+check_python
+
+Push-Location src/generator/godot_js
+
+& $python js_generator.py
+
+Pop-Location
+
+
 scons use_mingw=true generate_template_get_node=false
